865ef1b926a4819c1bb2a146a6aec819fafa6409
lolololol <lol@lol.com>
2026-06-08 10:40:10 +1000

    docs, .select syntax

--- README.md
+++ README.md
@@ -4,7 +4,33 @@
 
 Docs: [database](docs/DB.md)
 
-Vaporeon is a simple tree-sitter based Python app, which reads through a directory of C code and attempts to create a call database (of which functions call which other functions).
+Vaporeon is a simple tree-sitter based Python app, which reads through a directory of C code and attempts to create a call database (of which functions call which other functions). To use this, start by indexing a folder of source code:
+
+```
+./vaporeon.py -i ~/data/qemu -d ~/data/qemu_src.db"
+```
+
+Now, you can access the database and attempt to map "hot paths" through a codebase:
+
+```
+./vaporeon.py -l ~/data/qemu_src.db
+ > .xrefs_from transaction_action
+info: called .xrefs_from('transaction_action')
+(446, 59, 'transaction_action', 'external_snapshot_action')
+(447, 59, 'transaction_action', 'drive_backup_action')
+(448, 59, 'transaction_action', 'blockdev_backup_action')
+(449, 59, 'transaction_action', 'abort_action')
+(450, 59, 'transaction_action', 'internal_snapshot_action')
+(451, 59, 'transaction_action', 'block_dirty_bitmap_add_action')
+(452, 59, 'transaction_action', 'block_dirty_bitmap_clear_action')
+(453, 59, 'transaction_action', 'block_dirty_bitmap_enable_action')
+(454, 59, 'transaction_action', 'block_dirty_bitmap_disable_action')
+(455, 59, 'transaction_action', 'block_dirty_bitmap_merge_action')
+(456, 59, 'transaction_action', 'block_dirty_bitmap_remove_action')
+(457, 59, 'transaction_action', 'g_assert_not_reached')
+```
+
+This identifies all (single-depth) calls made from transaction_action.
 
 It is not perfect by a long shot - but right now, we do not care, because it is better than having AI stumble through files with ripgrep and glob.
 
--- core/index.py
+++ core/index.py
@@ -7,36 +7,43 @@ from typing import Annotated
 conn = None
 cur = None
 
-def paths_to(fn: Annotated[str, "Function to get paths to"]):
-  x = _path_to(fn,[],10)
-  print(x)
-  return x
-
-def _path_to(fn,callpath,max_depth):
-  data = xrefs_to(fn)
-  if len(data) == 0 or max_depth == 0:
-    return [fn] + callpath
-  else:
-    children = []
-    for (id,srcid,src,dest) in data:
-      children.append(dest)
-    return [_path_from(kid,[fn] + callpath,max_depth - 1) for kid in children]
-  
-
 def paths_from(fn: Annotated[str, "Function to get paths from"]):
-  x = _path_from(fn,[],10)
-  print(x)
-  return x
+  paths = []  
+  def _path_from(fn,callpath,max_depth):
+    data = xrefs_from(fn)
+    if len(data) == 0 or max_depth == 0:
+      paths.append("->".join(callpath + [fn]))
+    else:
+      children = []
+      for (id,srcid,src,dest) in data:
+        children.append(dest)
+      for c in children:
+        if c in callpath or c == fn:
+          continue
+        _path_from(c,callpath + [fn],max_depth - 1)
+  _path_from(fn,[],10)
+  print(paths)
+  return paths
+  # return x
 
-def _path_from(fn,callpath,max_depth):
-  data = xrefs_from(fn)
-  if len(data) == 0 or max_depth == 0:
-    return callpath + [fn]
-  else:
-    children = []
-    for (id,srcid,src,dest) in data:
-      children.append(dest)
-    return [_path_from(kid,callpath + [fn],max_depth - 1) for kid in children]
+def paths_to(fn: Annotated[str, "Function to get paths to"]):
+  paths = []  
+  def _path_to(fn,callpath,max_depth):
+    data = xrefs_to(fn)
+    if len(data) == 0 or max_depth == 0:
+      paths.append("->".join([fn] + callpath))
+    else:
+      children = []
+      for (id,srcid,src,dest) in data:
+        children.append(dest)
+      for c in children:
+        if c in callpath or c == fn:
+          continue
+        _path_to(c,[fn] + callpath,max_depth - 1)
+  _path_to(fn,[],10)
+  print(paths)
+  return paths
+  # return x
 
 def _load_index(filename):
   global conn, cur
@@ -64,7 +71,7 @@ xrefs_to.__doc__ = "Find what calls a function."
 def run_cli():
   while True:
     in_cmd = input(" > ").strip()
-    if in_cmd.startswith("!"):
+    if in_cmd.startswith(".select"):
       print("info: pass directly to sqlite3")
       cur.execute(in_cmd[1:])
       if cur.description is not None:
